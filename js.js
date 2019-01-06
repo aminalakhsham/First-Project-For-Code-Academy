c5a15ff6e971161e9419d0662dc7bfd9fe83a942
aminalakhsham <42694796+aminalakhsham@users.noreply.github.com>
2019-01-06 21:03:59 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  if (window.pageYOffset <= 500) {
+  if (window.pageYOffset <= 550) {
     document.getElementsByClassName("navbar")[0].style.background = "none";
   } else {
     document.getElementsByClassName("navbar")[0].style.background = "#000";
